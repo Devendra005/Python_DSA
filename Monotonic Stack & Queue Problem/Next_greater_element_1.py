7a7ff26357adc4b70923b753306cd99119563356
Devendra005 <156700585+Devendra005@users.noreply.github.com>
2026-02-15 20:48:19 +0530

    Add implementations for asteroid collision and next greater element problems

---
+++ Monotonic Stack & Queue Problem/Next_greater_element_1.py
@@ -0,0 +1,23 @@
+# Brute solution
+
+nums = [19, 4, 2, 11, 6, 5, 3, 10]
+n = len(nums)
+ans = [-1] * n
+for i in range(0, n):
+    for j in range(i+1, n):
+        if nums[j] > nums[i]:
+            ans[i] = nums[j]
+            break
+print(ans)
+
+
+# Optimal solution using stack
+result = [-1] * n
+stack = []
+for i in range(n-1, -1, -1):
+    while len(stack) != 0 and stack[-1] <= nums[i]:
+        stack.pop()
+    if len(stack) != 0:
+        result[i] = stack[-1]
+    stack.append(nums[i])
+print(result)
